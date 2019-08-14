ce7359a4d0016c4d44785b1657a24beccfd34322
veroandreo <veroandreo@gmail.com>
2019-08-14 03:25:29 +0200

    WIP - code for space-time variable extraction for disease ecology presentation

--- code/grass_R_disease_ecology_code.sh
+++ code/grass_R_disease_ecology_code.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 ########################################################################
-# Commands for GRASS - R interface exercise: 
+# GRASS GIS commands for the session: 
+# "Analysis of space-time satellite data for disease ecology 
+# applications with GRASS GIS and R stats" at OpenGeoHub Summer School,
+# Muenster (Germany)
+#
 # Modelling Aedes albopictus potential distribution in Northern Italy
+#
 # Author: Veronica Andreo
+#
 # Date: August, 2019
 ########################################################################
 
@@ -93,8 +99,10 @@ t.rast.algebra \
 # Generate environmental variables from LST STRDS
 #
 
+
+## Bioclimatic variables
   
-# Monthly climatology to feed r.bioclim
+# Monthly avg, min and max LST climatology to feed r.bioclim
 for i in `seq -w 1 12` ; do 
   
   # average
@@ -117,8 +125,6 @@ for i in `seq -w 1 12` ; do
 
 done
 
-# Bioclimatic variables
-
 # Install extension
 g.extension extension=r.bioclim
  
@@ -129,6 +135,8 @@ r.bioclim \
   tavg=`g.list type=raster pattern="lst_average_??" separator=,` \
   toutscale=1  
 
+
+## Spring warming
 
 # Annual spring warming: slope(daily Tmean february-march-april)
 t.rast.aggregate input=lst_daily_celsius \
@@ -145,6 +153,8 @@ t.rast.series input=annual_spring_warming \
   method=average
 
 
+## Autumnal cooling
+
 # Annual autumnal cooling: slope(daily Tmean august-september-october)
 t.rast.aggregate input=lst_daily_celsius \
   output=annual_autumnal_cooling \
@@ -160,21 +170,21 @@ t.rast.series input=annual_autumnal_cooling \
   method=average
 
 
-# Start/end and length of mosquito growing season
+## Start/end and length of mosquito growing season
 
 # Install extension
 g.extension extension=r.seasons
 
-YEARS=`seq 2010 2018`
-
-for YEAR in ${YEARS[*]} ; do 
-
+# Detect seasons
+for YEAR in `seq 2010 2018` ; do 
+  
+  # Lists per year
   t.rast.list -u lst_daily_celsius \
     columns=name \
     where="strftime('%Y',start_time)='${YEAR}'" \
     output=list_${YEAR}.txt
 
-  # mosquito season (threshold=10C, min duration 150 days)
+  # Mosquito season (threshold: 10C, min duration: 150 days)
   r.seasons file=list_${YEAR} \
     prefix=mosq_season_${YEAR}_ \
     n=1 \
@@ -183,64 +193,45 @@ for YEAR in ${YEARS[*]} ; do
     min_length=150 \
     max_gap=12
 
-  # length
+  # Length core season
   r.mapcalc expression="mosq_season_length_${YEAR}_1 = \
     mosq_season_${YEAR}_1_end1 - mosq_season_${YEAR}_1_start1 + 1"
   
+  # Length extended season
   r.mapcalc expression="mosq_season_length_${YEAR}_2 = \
     mosq_season_${YEAR}_1_end2 - mosq_season_${YEAR}_1_start2 + 1" 
-
-  # WNV transmision season (threshold(EIPmedian)=14.3, min duration 120 days)
-  r.seasons file=list_${YEAR} \
-    prefix=wnv_season_eip50_${YEAR}_ \
-    n=1 \
-    nout=wnv_season_eip50_${YEAR} \
-    threshold=14.3 \
-    min_length=120 \
-    max_gap=12
- 
-  # length
-  r.mapcalc expression="wnv_season_length_eip50_${YEAR}_1 = \
-    wnv_season_eip50_${YEAR}_1_end1 - wnv_season_eip50_${YEAR}_1_start1 + 1"
-  
-  r.mapcalc expression="wnv_season_length_eip50_${YEAR}_2 = \
-    wnv_season_eip50_${YEAR}_1_end2 - wnv_season_eip50_${YEAR}_1_start2 + 1" 
  
 done
 
-# Median start and end of season
-for i in "mosq" "wnv" ; do 
+# Start and end of core season
+r.series \
+  input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_start1` \
+  output=mosq_season_median_start1 \
+  method=median
 
-  # core season
-  r.series \
-    input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_start1` \
-    output=${i}_season_median_start1 \
-    method=median
+r.series \
+  input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_end1` \
+  output=mosq_season_median_end1 \
+  method=median
 
-  r.series \
-    input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_end1` \
-    output=${i}_season_median_end1 \
-    method=median
+# Start and end of extended season
+r.series \
+  input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_start2` \
+  output=mosq_season_median_start2 \
+  method=median
 
-  # extended season
-  r.series \
-    input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_start2` \
-    output=${i}_season_median_start2 \
-    method=median
-
-  r.series \
-    input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_end2` \
-    output=${i}_season_median_end2 \
-    method=median
- 
-done
+r.series \
+  input=`g.list type=raster sep=comma pattern=${i}_season_20??_1_end2` \
+  output=mosq_season_median_end2 \
+  method=median
 
 
-# Number of days with Tmean >= 20 <= 30 (optimal develop T mosquitoes)
+## Number of days with LSTmean >= 20 <= 30
 
 # Keep only pixels meeting the condition
 t.rast.algebra \
-  expression="tmean_higher20_lower30 = if(lst_daily_celsius >= 20 && lst_daily <= 30, lst_daily_celsius, null())" \
+  expression="tmean_higher20_lower30 = if(lst_daily_celsius >= 20 && \
+  lst_daily <= 30, lst_daily_celsius, null())" \
   basename=tmean_higher20_lower30 \
   suffix=gran
 
@@ -258,7 +249,7 @@ t.rast.list \
   columns=name,start_time,end_time,min,max
 
 
-# Number of consecutive days with Tmean >= 35
+## Number of consecutive days with LSTmean >= 35
 
 # Create annual mask
 t.rast.aggregate input=lst_daily \
@@ -267,9 +258,11 @@ t.rast.aggregate input=lst_daily \
   granularity="1 year" \
   method=count
 
-# Calculate consecutive days with Tmean >= 35
+# Calculate consecutive days with LSTmean >= 35
 t.rast.algebra basename=higher35_days suffix=gran \
-  expression="higher35_consec_days = annual_mask {+,contains,l} if(lst_daily_celsius >= 35.0 && lst_daily_celsius[-1] >= 35.0 || lst_daily_celsius[1] >= 35.0 && lst_daily_celsius >= 35.0, 1, 0)"
+  expression="higher35_consec_days = annual_mask {+,contains,l} \
+  if(lst_daily_celsius >= 35.0 && lst_daily_celsius[-1] >= 35.0 || \
+  lst_daily_celsius[1] >= 35.0 && lst_daily_celsius >= 35.0, 1, 0)"
 
 # Inspect values
 t.rast.list input=higher35_consec_days \
@@ -281,7 +274,7 @@ t.rast.series input=higher35_consec_days \
   method=median
 
 
-# Growing Degree Days
+## Growing Degree Days
 
 # Accumulation of degree days
 t.rast.accumulate -n input=lst_daily_celsius \
@@ -293,13 +286,15 @@ t.rast.accumulate -n input=lst_daily_celsius \
   granularity="1 day" \
   method=bedd limits=10,30
 
-t.info mosq_daily_bedd
+# Get basic info
+t.info input=mosq_daily_bedd
 
-t.rast.list mosq_daily_bedd \
+# Inspect values
+t.rast.list input=mosq_daily_bedd \
   columns=name,start_time,end_time,min,max
 
 
-# Mosquito cycles detection (Full cycle: 1350 DD)
+## Detection of mosquito cycles (Full cycle: 1350 DD)
 
 # arrays
 cycle=(`seq 1 5`)
@@ -313,7 +308,7 @@ for i in `seq 1 $count` ; do
 
   echo "cycle: "${cycle[$i-1]}" - "${cycle_beg[$i-1]} ${cycle_end[$i-1]}
 
-  # identify cycles
+  # Identify cycles
   t.rast.accdetect input=mosq_daily_bedd \
     occurrence=mosq_occurrence_cycle${cycle[$i-1]} \
     indicator=mosq_indicator_cycle${cycle[$i-1]} \
@@ -322,10 +317,10 @@ for i in `seq 1 $count` ; do
     cycle="7 months" offset="5 months" \
     range=${cycle_beg[$i-1]},${cycle_end[$i-1]}
 
-  # yearly spatial occurrence 
+  # Yearly spatial occurrence 
 
-  # extract areas that have full cycles
-  # indicator takes 1, 2 or 3 for start, middle and end of cycle
+  # Extract areas that have full cycles
+  # Indicator takes values 1, 2 or 3 for start, middle and end of cycle
   t.rast.aggregate input=mosq_indicator_cycle${cycle[$i-1]} \
     output=mosq_cycle${cycle[$i-1]}_yearly \
     basename=mosq_c${cycle[$i-1]}_yearly \
@@ -333,15 +328,14 @@ for i in `seq 1 $count` ; do
     method=maximum \
     suffix=gran
 
-  # keep only complete cycles
+  # Keep only complete cycles
   t.rast.mapcalc input=mosq_cycle${cycle[$i-1]}_yearly \
     output=mosq_cycle${cycle[$i-1]}_yearly_clean \
     basename=mosq_clean_c${cycle[$i-1]} \
     expression="if(mosq_cycle${cycle[$i-1]}_yearly == 3, ${cycle[$i-1]}, null())"
   
-  # duration of the mosquito cycle
-	
-  # beginning
+  # Duration of the mosquito cycle
+  # Beginning
   t.rast.aggregate input=mosq_occurrence_cycle${cycle[$i-1]} \
     output=mosq_min_day_cycle${cycle[$i-1]} \
     basename=occ_min_day_c${cycle[$i-1]} \
@@ -349,7 +343,7 @@ for i in `seq 1 $count` ; do
     granularity="1 year" \
     suffix=gran
 	
-  # end
+  # End
   t.rast.aggregate input=mosq_occurrence_cycle${cycle[$i-1]} \
     output=mosq_max_day_cycle${cycle[$i-1]} \
     basename=occ_max_day_c${cycle[$i-1]} \
@@ -357,7 +351,7 @@ for i in `seq 1 $count` ; do
     granularity="1 year" \
     suffix=gran
 	
-  # difference
+  # Difference
   t.rast.mapcalc input=mosq_min_day_cycle${cycle[$i-1]},mosq_max_day_cycle${cycle[$i-1]} \
     output=mosq_duration_cycle${cycle[$i-1]} \
     basename=mosq_duration_cycle${cycle[$i-1]} \
